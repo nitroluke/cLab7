bb36310ebbc37e10a23557928c80b02bdbbccf98
nitroluke <nitro.luke@gmail.com>
2014-02-26 15:49:15 -0700

    sftp from esus with simple file read in to array and unfinished cypher

--- decodeCypherMessage.c
+++ decodeCypherMessage.c
@@ -1,43 +1,76 @@
-/* Luke Welna
+/* 
+Luke Welna
 2-25-14
-in lab 7
+lab 07
 */
 
-#include <stdlib.h>
 #include <stdio.h>
-
+char itsCharCounterpart[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+int anIntCypher[26] = { }; /* changed this to type enum, could cause problems */
+int sizeOfMessage = 14;
 void assignToChar();
+void decodeTheMessage();
 
 int main(){
 assignToChar();
-return 0; 
+return 0;
 }
 
 /*
-Reads in integers from a text file, takes those values and gives them 
-an associated char.
+reads in a text file and adds that to an array called anIntCypher[].
 */
 void assignToChar(){
-int positionInArray = 0; 
-File *readInInt = fopen("cipher.txt", "r");
+int positionInArray = 0;
+FILE *readInInt = fopen("cypher.txt", "r");
+int tempArrayNum = 0;;
 
-int anIntCypher[26];
-char itsCharCounterpart[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-'w', 'x', 'y', 'z'};
-int tempArrayNum;
-
-fscanf();
-	while(fscanf(readInInt, "%d, ",&tempArrayNum) == 1){ 
+	while(fscanf(readInInt, "%d, ", &tempArrayNum) == 1){
 	anIntCypher[positionInArray] = tempArrayNum;
 	positionInArray++;
 	}
-	/*This is temporary for testing thats why im using i and not a 3 word variable*/
-	for(int i = 0;  i < 26; i++){  
-	printf("Position &i = %i", i,&anIntCypher[i]);
+
+/*using i instead of three words because this is for testing
+int i = 0;
+
+	for(i; i<26; ++i){
+	printf("The address of anIntCypher[%i] = %i\n", i, &anIntCypher[i]);
+	printf("               anIntCypher[%i] = %i\n", i, anIntCypher[i]);
 	}
-	
-	
+*/
+decodeTheMessage();
 }
 
-/*To write out to a file just use fopen but instead of "r" use "w"  */
+void decodeTheMessage(){
+int tempArrayNum = 0;
+/* int messageToDecode[] = { }; */
+int positionInArray = 0;
+FILE *readInMessage = fopen("messages.txt", "r");
+FILE *writeOutMessage = fopen("decode.txt", "w");
+
+if(!(readInMessage && writeOutMessage)){
+printf("failed to open files\n");
+exit(-1); /* This will exit the program for testing */
+}
+
+int isScanInt = 1;
+	while(isScanInt != 0){ /*changed this from -1 to 0 */
+	isScanInt = fscanf(readInMessage, "%d", &tempArrayNum);
+
+		if(isScanInt == 0){  
+		puts("isScanInt == 0");
+		fscanf(readInMessage, "%c");
+		}else{
+		printf("tempArrayNum = %i\n", tempArrayNum); 
+		/* messageToDecode[positionInArray] = tempArrayNum; */
+		/*fprintf(writeOutMessage, "%c" , itsCharCounterpart[tempArrayNum]);*/
+		}
+	}
+
+
+/*	int thisIsCounter = 0;
+	for(thisIsCounter; thisIsCounter < sizeOfMessage; thisIsCounter++){
+	printf(itsCharCounterpart[messageToDecode[thisIsCounter]]); 
+	fscanf
+	}
+*/
+}
